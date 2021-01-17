67998b8ce0081a2fe3a8ab2cab63bad5974fc7f7
Tomasz Formela <alemrof@gmail.com>
2021-01-17 15:28:03 +0100

    Dodano mozliwosc przelaczenia sie po okienkach w zagregowanych obiektach

--- project/public/js/map.js
+++ project/public/js/map.js
@@ -1,8 +1,14 @@
+let selectedWarehouses = null;
+let selectedWarehouseNumber = 0;
+
 // Tworzenie źródła na podstawie danych przesłanych z bazy danych
 let vectorSource = new ol.source.Vector();
 for(let warehouse of warehouses) {
     let pointFeature = new ol.Feature({
         geometry: new ol.geom.Point(ol.proj.fromLonLat(warehouse.location.coordinates)),
+        name: warehouse.name,
+        company: warehouse.company.name,
+        address: warehouse.address
     });
     pointFeature.setId(warehouse.id);
     vectorSource.addFeature(pointFeature);
@@ -31,19 +37,25 @@ let clusterLayer = new ol.layer.Vector({
         let style = styleCache[size];
         if (!style) {
             style = new ol.style.Style({
-                image: new ol.style.Circle({
-                    radius: 10,
-                    stroke: new ol.style.Stroke({
-                        color: '#fff',
-                    }),
-                    fill: new ol.style.Fill({
-                        color: '#3399CC',
-                    }),
+                image: new ol.style.Icon({
+                    src: '../imgs/warehouse-icon-20px.png',
                 }),
+                // image: new ol.style.Circle({                 // Alternatywny wygląd
+                //     radius: 10,
+                //     stroke: new ol.style.Stroke({
+                //         color: '#fff',
+                //     }),
+                //     fill: new ol.style.Fill({
+                //         color: '#3399CC',
+                //     }),
+                // }),
                 text: new ol.style.Text({
                     text: size.toString(),
+                    font: '14px arial',
+                    textAlign: 'center',
+                    offsetY: -15,
                     fill: new ol.style.Fill({
-                        color: '#fff',
+                        color: '#000',
                     }),
                 }),
             });
@@ -60,8 +72,8 @@ var map = new ol.Map({
         new ol.layer.Tile({
             source: new ol.source.OSM()
         }),
-        vectorLayer,
-        // clusterLayer,
+        // vectorLayer,
+        clusterLayer,
     ],
     view: new ol.View({
     center: ol.proj.fromLonLat([18.65, 54.35]),
@@ -83,8 +95,27 @@ map.addOverlay(popup);
 let closer = document.querySelector('#closer');
 closer.addEventListener('click', function(e){
     popup.setPosition(undefined);
+    selectedWarehouses = null;
+    selectedWarehouseNumber = 0;
     return false;
 });
+
+function updatePopup(id) {
+    let warehouseName = document.querySelector('#warehouse-name');
+    warehouseName.innerHTML = selectedWarehouses[id].get('name');
+    
+    let warehouseCompany = document.querySelector('#warehouse-company');
+    warehouseCompany.innerHTML = selectedWarehouses[id].get('company');
+    
+    let warehouseAddress = document.querySelector('#warehouse-address');
+    warehouseAddress.innerHTML = selectedWarehouses[id].get('address');
+
+    let editLink = document.querySelector('#edit-link');
+    editLink.href = `http://localhost:8000/warehouses/${selectedWarehouses[id].getId()}/edit`;
+
+    let editMapLink = document.querySelector('#editMap-link');
+    editMapLink.href = `http://localhost:8000/warehouses/${selectedWarehouses[id].getId()}/editMap`;
+}
 
 // Wybieranie składu
 var select = new ol.interaction.Select({
@@ -93,41 +124,43 @@ var select = new ol.interaction.Select({
 
 map.addInteraction(select);
 
-function findWarehouse(id) {
-    for(let warehouse of warehouses) {
-        if (warehouse.id == id) {
-            return warehouse;
-        }
-    }
-    return null;
-}
-
 select.on('select', function(e) {
-    // for (let i = 0; i < e.target.getFeatures().item(0).get('features').length; i++) {
-    //     console.log(e.target.getFeatures().item(0).get('features')[i].getId());
-    // }
     if (e.target.getFeatures().item(0)) {
-        let selectedWarehouse = findWarehouse(e.target.getFeatures().item(0).getId())
-        if (selectedWarehouse !== null) {
+        selectedWarehouses = e.target.getFeatures().item(0).get('features');
+        selectedWarehouseNumber = 0;
+
+        if (selectedWarehouses) {
+            let popupArrows = document.querySelector('#popupArrows');
+            if (selectedWarehouses.length > 1) {
+                popupArrows.classList.add('d-flex');
+                popupArrows.classList.remove('d-none');
+
+            } else {
+                popupArrows.classList.add('d-none');
+                popupArrows.classList.remove('d-flex');
+            }
+
             let coordinates = e.target.getFeatures().item(0).getGeometry().getCoordinates();
             popup.setPosition(coordinates);
-            let warehouseName = document.querySelector('#warehouse-name');
-            warehouseName.innerHTML = selectedWarehouse.name;
-            let warehouseCompany = document.querySelector('#warehouse-company');
-            warehouseCompany.innerHTML = selectedWarehouse.company.name;
-            let warehouseLocation = document.querySelector('#warehouse-location');
-            let firstCoordinate = Math.round(selectedWarehouse.location.coordinates[0] * 100000) / 100000;
-            let secondCoordinate = Math.round(selectedWarehouse.location.coordinates[1] * 100000) / 100000;
-            warehouseLocation.innerHTML = firstCoordinate + ', ' + secondCoordinate;
             
-            let editLink = document.querySelector('#edit-link');
-            editLink.href = `http://localhost:8000/warehouses/${selectedWarehouse.id}/edit`;
-
-            let editMapLink = document.querySelector('#editMap-link');
-            editMapLink.href = `http://localhost:8000/warehouses/${selectedWarehouse.id}/editMap`;
+            // let warehouseName = document.querySelector('#warehouse-name');
+            // warehouseName.innerHTML = selectedWarehouses[selectedWarehouseNumber].get('name');
+            
+            // let warehouseCompany = document.querySelector('#warehouse-company');
+            // warehouseCompany.innerHTML = selectedWarehouses[selectedWarehouseNumber].get('company');
+            
+            // let warehouseAddress = document.querySelector('#warehouse-address');
+            // warehouseAddress.innerHTML = selectedWarehouses[selectedWarehouseNumber].get('address');
+            updatePopup(selectedWarehouseNumber);
+            
+            let warehouseLocation = document.querySelector('#warehouse-location');
+            let firstCoordinate = Math.round(ol.proj.toLonLat(coordinates)[0] * 100000) / 100000;
+            let secondCoordinate = Math.round(ol.proj.toLonLat(coordinates)[1] * 100000) / 100000;
+            warehouseLocation.innerHTML = firstCoordinate + ', ' + secondCoordinate;
         }
     } else {
         popup.setPosition(undefined);
+        selectedWarehouseNumber = 0;
     }
     
 });
@@ -141,3 +174,28 @@ var draw = new ol.interaction.Draw({
 vectorSource.on('addfeature', function(e) {
     map.removeInteraction(draw);
 });
+
+// Przyciski umożliwające przełączenia danych w okienku po wybraniu klastru punktów
+let showNextWarehouse = document.querySelector('#popupNextWarehouse');
+showNextWarehouse.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (selectedWarehouses.length > 1) {
+        selectedWarehouseNumber = (selectedWarehouseNumber + 1) % selectedWarehouses.length;
+        updatePopup(selectedWarehouseNumber);
+    } else {
+        selectedWarehouseNumber = 0;
+    }
+})
+
+let showPreviousWarehouse = document.querySelector('#popupPreviousWarehouse');
+showPreviousWarehouse.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (selectedWarehouseNumber > 0) {
+        selectedWarehouseNumber--;
+        updatePopup(selectedWarehouseNumber);
+    } else {
+        selectedWarehouseNumber = selectedWarehouses.length - 1;
+        updatePopup(selectedWarehouseNumber);
+    }
+        
+})
