17d48b84118352c2828eea9621247243eea5863d
Przemyslaw Glebocki <glebocki.przemyslaw@gmail.com>
2021-01-19 13:37:38 +0100

    Move db example data generation into DatabaseSeeder

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\Warehouse;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name'=>'Tomasz Formela',
+            'email'=>'tomek@tomek.com', 
+            'password'=>Hash::make('tomek123')
+        ]);
+        Company::create(['name'=>'Castorama']);
+        Company::create(['name'=>'Leroy Merlin']);
+        Warehouse::create([
+            'name'=>'Castorama Gdańsk Oliwa',
+            'company_id'=>1,
+            'address'=>'aleja Grunwaldzka 262, 80-314 Gdańsk',
+            'location'=>new Point(54.396006808244124, 18.577674827404387)
+        ]);
+        Warehouse::create([
+            'name'=>'Castorama Odyseusza',
+            'company_id'=>1,
+            'address'=>'Odyseusza 2, 80-299 Gdańsk',
+            'location'=>new Point(54.43233463765607, 18.486433086502654)
+        ]);
+        Warehouse::create([
+            'name'=>'Leroy Merlin Gdańsk Oliwa',
+            'company_id'=>2,
+            'address'=>'aleja Grunwaldzka 309, 80-309 Gdańsk',
+            'location'=>new Point(54.39463073834571, 18.58101521551848)
+        ]);
+        Warehouse::create([
+            'name'=>'Leroy Merlin Gdańsk',
+            'company_id'=>2,
+            'address'=>'Szczęśliwa 7, 80-176 Gdańsk',
+            'location'=> new Point(54.35313340101314, 18.521470337609315)
+        ]);
     }
 }
--- project/routes/web.php
+++ project/routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use App\Models\Company;
-use App\Models\Warehouse;
-use Grimzy\LaravelMysqlSpatial\Types\Point;
-use Illuminate\Support\Facades\Hash;
-
+use Database\Seeders\DatabaseSeeder;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,36 +23,6 @@ Route::post('/warehouses/{id}/updateMap', [App\Http\Controllers\WarehouseControl
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/generate-data', function() {
-    User::create([
-        'name'=>'Tomasz Formela',
-        'email'=>'tomek@tomek.com', 
-        'password'=>Hash::make('tomek123')
-    ]);
-    Company::create(['name'=>'Castorama']);
-    Company::create(['name'=>'Leroy Merlin']);
-    Warehouse::create([
-        'name'=>'Castorama Gdańsk Oliwa',
-        'company_id'=>1,
-        'address'=>'aleja Grunwaldzka 262, 80-314 Gdańsk',
-        'location'=>new Point(54.396006808244124, 18.577674827404387)
-    ]);
-    Warehouse::create([
-        'name'=>'Castorama Odyseusza',
-        'company_id'=>1,
-        'address'=>'Odyseusza 2, 80-299 Gdańsk',
-        'location'=>new Point(54.43233463765607, 18.486433086502654)
-    ]);
-    Warehouse::create([
-        'name'=>'Leroy Merlin Gdańsk Oliwa',
-        'company_id'=>2,
-        'address'=>'aleja Grunwaldzka 309, 80-309 Gdańsk',
-        'location'=>new Point(54.39463073834571, 18.58101521551848)
-    ]);
-    Warehouse::create([
-        'name'=>'Leroy Merlin Gdańsk',
-        'company_id'=>2,
-        'address'=>'Szczęśliwa 7, 80-176 Gdańsk',
-        'location'=> new Point(54.35313340101314, 18.521470337609315)
-    ]);
+    (new DatabaseSeeder())->run();
     echo 'data generated';
 });
