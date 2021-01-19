a029192dfef584b8c7a067ddbfaaca0ebfef111b
Przemyslaw Glebocki <glebocki.przemyslaw@gmail.com>
2021-01-19 13:30:30 +0100

    Update details of example warehouses

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use App\Models\Company;
-use App\Models\Warehouse;
-use Grimzy\LaravelMysqlSpatial\Types\Point;
-use Illuminate\Support\Facades\Hash;
+use Database\Seeders\DatabaseSeeder;
 
 
 /*
@@ -28,36 +24,6 @@ Route::post('/warehouses/{id}/updateMap', [App\Http\Controllers\WarehouseControl
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/generate-data', function() {
-    User::create([
-        'name'=>'Tomasz Formela',
-        'email'=>'tomek@tomek.com', 
-        'password'=>Hash::make('tomek123')
-    ]);
-    Company::create(['name'=>'Castorama']);
-    Company::create(['name'=>'Leroy Merlin']);
-    Warehouse::create([
-        'name'=>'Skład 1',
-        'company_id'=>1,
-        'address'=>'ul. Grunwaldzka 149, 81-450 Gdańsk',
-        'location'=>new Point(54.396006808244124, 18.577674827404387)
-    ]);
-    Warehouse::create([
-        'name'=>'Skład 2',
-        'company_id'=>1,
-        'address'=>'ul. Grunwaldzka 149, 81-450 Gdańsk',
-        'location'=>new Point(54.43233463765607, 18.486433086502654)
-    ]);
-    Warehouse::create([
-        'name'=>'Skład 1',
-        'company_id'=>2,
-        'address'=>'ul. Grunwaldzka 149, 81-450 Gdańsk',
-        'location'=>new Point(54.39463073834571, 18.58101521551848)
-    ]);
-    Warehouse::create([
-        'name'=>'Skład 2',
-        'company_id'=>2,
-        'address'=>'ul. Grunwaldzka 149, 81-450 Gdańsk',
-        'location'=> new Point(54.35313340101314, 18.521470337609315)
-    ]);
+    (new DatabaseSeeder())->run();
     echo 'data generated';
 });
