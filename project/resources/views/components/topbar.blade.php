29fd71eaae0ac7d3c5737af6faa5b00c57e1f37d
Tomasz Formela <alemrof@gmail.com>
2021-01-30 08:57:30 +0100

    added warehouse searching feature

--- project/resources/views/components/topbar.blade.php
+++ project/resources/views/components/topbar.blade.php
@@ -6,17 +6,7 @@
     <i class="fa fa-bars"></i>
   </button>
 
-  <!-- Topbar Search -->
-  {{-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-    <div class="input-group">
-      <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-      <div class="input-group-append">
-        <button class="btn btn-dark" type="button">
-          <i class="fas fa-search fa-sm"></i>
-        </button>
-      </div>
-    </div>
-  </form> --}}
+  @yield('topbar-search')
 
   <!-- Topbar Navbar -->
   <ul class="navbar-nav ml-auto">
@@ -44,7 +34,7 @@
               <a class="dropdown-item" href="{{ route('logout') }}"
                   onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
+                  {{ __('Wyloguj') }}
               </a>
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
