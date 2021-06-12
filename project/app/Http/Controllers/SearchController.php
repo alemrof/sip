d2a2f201eb2c66a54c5eac5add1bc0dba8d8ccc7
Michal <polibudamichal@gmail.com>
2021-06-13 01:58:42 +0200

    Corrected initial products list on product search page startup

--- project/app/Http/Controllers/SearchController.php
+++ project/app/Http/Controllers/SearchController.php
@@ -26,7 +26,27 @@ class SearchController extends Controller
         $openingHours = OpeningHours::all();
         $companies = Company::all();
         $categories = Category::all();
-        $products = DB::select(' select distinct name  FROM products as p' );
+        $products = DB::select(DB::raw('
+                            select distinct name from
+                                        (SELECT c.name as category, k.name, k.manufacturer,k.company_id,k.price,k.warehouse_id,ST_X(k.location) as x, ST_Y(k.location) as y
+                                        from categories as c
+                                        INNER JOIN
+                                            (SELECT  p.name, p.category_id, p.manufacturer,pww.company_id ,pww.price,pww.warehouse_id,pww.location
+                                            FROM products as p
+                                            INNER JOIN
+                                                (select pwh.warehouse_id, pwh.product_id,pwh.price,w.company_id,w.location
+                                                    from product_warehouse as pwh
+                                                    inner join warehouses as w
+                                                    on pwh.warehouse_id = w.id
+                                                ) as pww
+                                            ON p.id = pww.product_id
+                                            ) as k
+                                        ON c.id = k.category_id) as j
+                            INNER JOIN
+                             (select companies.id,companies.name as company from companies) as g
+                            on
+                            g.id=j.company_id
+                            WHERE (j.category = ? AND g.company = ?)'),["Cementy i Zaprawy", "Castorama"]);
 
           return view('search.index',['companies' => $companies, 'categories' => $categories, 'products' => $products,'warehouses' => $warehouses]);
     }
